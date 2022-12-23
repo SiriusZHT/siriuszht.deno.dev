d003603ee51f4c771d9ae370ed69d3e79ca72659
SiriusZHT <49780895+SiriusZHT@users.noreply.github.com>
2022-12-23 16:55:30 +0800

    feat: main.tsx

--- main.tsx
+++ main.tsx
@@ -1,14 +1,14 @@
 import blog from "https://deno.land/x/blog/blog.tsx";
 
 blog({
-  author: "Dino",
-  title: "My Blog",
-  description: "The blog description.",
+  author: "SiriusZHT",
+  title: "玉玉的 Blog",
+  description: "欢迎来到玉玉的网站！",
   avatar: "https://deno-avatar.deno.dev/avatar/83a531.svg",
   avatarClass: "rounded-full",
   links: [
     { title: "Email", url: "mailto:bot@deno.com" },
-    { title: "GitHub", url: "https://github.com/denobot" },
+    { title: "GitHub", url: "https://github.com/siriuszht" },
     { title: "Twitter", url: "https://twitter.com/denobot" },
   ],
   lang: "zh",
